52f90c81733038176dae933559d6896a40ebc783
kittop <k.kotegorenko@gmail.com>
2020-06-02 14:55:24 +0400

    Scaffold auth

---
+++ WebParking/Areas/Identity/Data/WebParkingUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace WebParking.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the WebParkingUser class
+    public class WebParkingUser : IdentityUser
+    {
+    }
+}
